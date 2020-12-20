ff84968c22f33c3186a012aead81f682e212cc09
wnsgur1198 <wnsgur1198@naver.com>
2020-12-20 23:08:12 +0900

    Updates

---
+++ precache-manifest.c7621102864d77329d2cafd23d3f014e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecfd8fb009103cc35e939b6e886d64b5",
+    "url": "/myfolio/index.html"
+  },
+  {
+    "revision": "57f232a27ab17eaa2eba",
+    "url": "/myfolio/static/css/main.7a95f607.chunk.css"
+  },
+  {
+    "revision": "f0585afd58bab4c12498",
+    "url": "/myfolio/static/js/2.9a2180c5.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/myfolio/static/js/2.9a2180c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57f232a27ab17eaa2eba",
+    "url": "/myfolio/static/js/main.fcaf9d24.chunk.js"
+  },
+  {
+    "revision": "13f38dde41db379988a8",
+    "url": "/myfolio/static/js/runtime-main.05518b59.js"
+  },
+  {
+    "revision": "b88924d5d914bf9f3885f87c2ed76477",
+    "url": "/myfolio/static/media/sad-icon.b88924d5.png"
+  },
+  {
+    "revision": "b9d7d448a09d6aacb45d21aab2c6fd53",
+    "url": "/myfolio/static/media/your photo.b9d7d448.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/myFolio/precache-manifest.0dd724f2862c79a681be9add2f02cd71.js"
+  "/myfolio/precache-manifest.c7621102864d77329d2cafd23d3f014e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myFolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
